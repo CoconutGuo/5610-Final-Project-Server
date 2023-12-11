d2d6a105a4b07b48236ba9bb637404e18c5263c9
zhongsijia5726@outlook.com <zhong.sij@northeastern.edu>
2023-12-10 18:41:28 -0800

    update11

--- app.js
+++ app.js
@@ -13,12 +13,26 @@ const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0
 mongoose.connect(CONNECTION_STRING)
 
 const app = express()
-app.use(
-  cors({
-    credentials: true,
-    origin: "https://main--charming-cucurucho-2fd0c4.netlify.app/",
-  })
-)
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: "https://main--charming-cucurucho-2fd0c4.netlify.app/",
+//   })
+// )
+const allowedDomain = "https://main--charming-cucurucho-2fd0c4.netlify.app";
+
+const corsOptions = {
+  credentials: true,
+  origin: function (origin, callback) {
+    if (!origin || origin.startsWith(allowedDomain)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 
 const sessionOptions = {
   secret: 'any string',
